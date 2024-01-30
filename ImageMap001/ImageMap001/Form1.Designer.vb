83db0879e51f21327a8dc451b2a31fe9bd82b965
ArtiCube <151802132+ArtiCube@users.noreply.github.com>
2024-01-30 17:38:06 -0600

    Add files via upload

---
+++ ImageMap001/ImageMap001/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label3 = New Label()
+        PictureBox2 = New PictureBox()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        TextBox2 = New TextBox()
+        ComboBox3 = New ComboBox()
+        RichTextBox1 = New RichTextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.Cuerpo_Maping
+        PictureBox1.Location = New Point(28, 28)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(557, 612)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(28, 652)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(44, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "RGB: "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(28, 696)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(100, 20)
+        Label3.TabIndex = 3
+        Label3.Text = "Coordenadas:"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.Cuerpo
+        PictureBox2.Location = New Point(28, 28)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(557, 612)
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(624, 28)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(109, 20)
+        Label4.TabIndex = 5
+        Label4.Text = "Región cuerpo:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(624, 51)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(216, 27)
+        TextBox1.TabIndex = 6
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(624, 101)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(42, 20)
+        Label5.TabIndex = 7
+        Label5.Text = "Tipo:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(624, 173)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(45, 20)
+        Label6.TabIndex = 9
+        Label6.Text = "Lado:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(624, 248)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(44, 20)
+        Label7.TabIndex = 11
+        Label7.Text = "Vista:"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(624, 330)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(72, 20)
+        Label8.TabIndex = 13
+        Label8.Text = "Cantidad:"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(624, 413)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(90, 20)
+        Label9.TabIndex = 15
+        Label9.Text = "Descripción:"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(624, 124)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 17
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"IZQUIERDO", "DERECHO"})
+        ComboBox2.Location = New Point(624, 196)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(151, 28)
+        ComboBox2.TabIndex = 18
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(624, 271)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(151, 27)
+        TextBox2.TabIndex = 19
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+        ComboBox3.Location = New Point(624, 353)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(151, 28)
+        ComboBox3.TabIndex = 20
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(624, 440)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(216, 183)
+        RichTextBox1.TabIndex = 21
+        RichTextBox1.Text = ""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(895, 726)
+        Controls.Add(RichTextBox1)
+        Controls.Add(ComboBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
+        Controls.Add(PictureBox2)
+        Controls.Add(Label3)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Private WithEvents TextBox1 As TextBox
+    Private WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+
+End Class
